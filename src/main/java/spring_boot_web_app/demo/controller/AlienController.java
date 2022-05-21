2fa4ac88b714ff584001a0721b94c411e472b649
Dhananjay <dhananjayman11@gmail.com>
2022-05-21 12:45:54 +0530

    REST crud operations

--- src/main/java/spring_boot_web_app/demo/controller/AlienController.java
+++ src/main/java/spring_boot_web_app/demo/controller/AlienController.java
@@ -1,17 +1,26 @@
 package spring_boot_web_app.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import spring_boot_web_app.demo.dao.AlienRepo;
 import spring_boot_web_app.demo.model.Alien;
 
-@Controller
+@RestController
 public class AlienController {
   @Autowired
   AlienRepo repo;
@@ -21,21 +30,31 @@ public class AlienController {
     return "home.jsp";
   }
 
-  @RequestMapping("/addAlien")
-  public String addAlien(Alien alien) {
+  @PostMapping("/alien")
+  public Alien addAlien(@RequestBody Alien alien) {
     repo.save(alien);
-    return "home.jsp";
+    return alien;
   }
 
-  @RequestMapping("/aliens")
-  @ResponseBody
-  public String getAliens() {
-    return repo.findAll().toString();
+  @GetMapping("/aliens")
+  public List<Alien> getAliens() {
+    return repo.findAll();
   }
 
-  @RequestMapping("/alien/{aid}")
-  @ResponseBody
-  public String getAlien(@PathVariable("aid") int aid) {
-    return repo.findById(aid).toString();
+  @GetMapping("/alien/{aid}")
+  public Optional<Alien> getAlien(@PathVariable("aid") int aid) {
+    return repo.findById(aid);
+  }
+
+  @PutMapping("/alien")
+  public Alien addOrUpdateAlien(@RequestBody Alien alien) {
+    repo.save(alien);
+    return alien;
+  }
+
+  @DeleteMapping("/alien/{aid}")
+  public String deleteAlien(@PathVariable("aid") int aid) {
+    repo.deleteById(aid);
+    return "Alien deletd";
   }
 }
--- src/main/java/spring_boot_web_app/demo/dao/AlienRepo.java
+++ src/main/java/spring_boot_web_app/demo/dao/AlienRepo.java
@@ -2,12 +2,13 @@ package spring_boot_web_app.demo.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.repository.CrudRepository;
 
 import spring_boot_web_app.demo.model.Alien;
 
-public interface AlienRepo extends CrudRepository<Alien, Integer> {
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
   List<Alien> findByTech(String tech);
 
   List<Alien> findByAidGreaterThan(int aid);
